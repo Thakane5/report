a05f7e0ccd4ea628643b2f3cd5678dc6e7ea10a7
Thakane5 <thakanesello4@gmail.com>
2025-10-03 23:10:58 +0200

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://report-backend-i8m1.onrender.com/";
 
 export const apiService = {
   // ==================== AUTH ENDPOINTS ====================
